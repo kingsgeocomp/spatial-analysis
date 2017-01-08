f97dd37f65a7affaa9048383dc5ae0fe7c03c092
Jon Reades <jon@reades.com>
2017-01-08 18:47:43 +0000

    Synching changes. Mainly focussed on AirBnB work.

--- AirBnB London Data.py
+++ AirBnB London Data.py
@@ -1,16 +1,36 @@
+import os
+import random
 import pandas as pd
 import seaborn as sb
+import geopandas as gpd
 
-df = pd.read_csv('~/Downloads/listings.csv')
+random.seed(123456789)
+
+df = pd.read_csv(os.path.join('data','airbnb-2016-Feb','listings-summary.csv.gz'))
 
 df.columns
 
 df.head(2)
 
+df.shape
+
 %pylab inline
 
-df.price.mean()
+print("Mean price: " + str(df.price.mean()))
+print("Mean number of reviews: " + str(df.number_of_reviews.mean()))
+
 sb.distplot(df.price)
 
-df.number_of_reviews.mean()
 sb.distplot(df.number_of_reviews)
+
+sb.distplot(df.availability_365)
+
+df.neighbourhood.unique()
+
+from shapely.geometry import Point
+crs = None
+geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
+gdf = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+gdf.crs = {'init' :'epsg:27700'}
+gdf.head()
+gdf.plot()
